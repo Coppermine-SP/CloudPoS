4a82b8e96dd3f5412a4297214bb654576d1f979b
cg-1119 <148218775+cg-1119@users.noreply.github.com>
2025-08-01 21:25:22 +0900

    feat: Created TableView

--- src/cloudpos/Pages/Administrative/TableView.razor.cs
+++ src/cloudpos/Pages/Administrative/TableView.razor.cs
@@ -1,14 +1,46 @@
 using CloudInteractive.CloudPos.Components.Modal;
+using CloudInteractive.CloudPos.Contexts;
 using CloudInteractive.CloudPos.Event;
+using CloudInteractive.CloudPos.Models;
 using CloudInteractive.CloudPos.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.EntityFrameworkCore;
 
 namespace CloudInteractive.CloudPos.Pages.Administrative;
 
-public partial class TableView(TableEventBroker broker, ModalService modal, ConfigurationService config) : ComponentBase
+public partial class TableView(
+    IDbContextFactory<ServerDbContext> factory, TableService service,
+    TableEventBroker broker, ModalService modal, ConfigurationService config) : ComponentBase
 {
-    private void Test()
-    {
-        
+    private ServerDbContext Context => factory.CreateDbContext();
+    private List<Table> _tables = [];
+    private Table? _selectedTable;
+    private TableSession? _selectedTableSession;
+    
+    
+    protected override async Task OnInitializedAsync()
+    { 
+        await LoadTablesAsync(); 
     }
+    
+    private async Task LoadTablesAsync()
+    { 
+        _tables = await Context.Tables.Include(t => t.Cell).ToListAsync();
+        StateHasChanged();
+    }
+
+    private async Task GetTableSessionAsync(int tableId)
+    {
+        _selectedTable = await Context.Tables.FirstOrDefaultAsync(t => t.TableId == tableId);
+        if (_selectedTable != null)
+        {
+            _selectedTableSession = await Context.Sessions
+                .Include(s => s.Orders)
+                .ThenInclude(o => o.OrderItems)
+                .ThenInclude(oi => oi.Item)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(s => s.TableId == tableId);
+        }
+    }
+    
 }
